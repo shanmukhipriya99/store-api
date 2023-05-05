3c34f6e793b5de97348edbeed4dd7ef212779342
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-05-04 18:59:27 -0700

    db connection

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('04 Store API')
+const express = require('express');
+require('dotenv').config();
+const AppError = require('./middleware/AppError');
+const { globalErrorHandler } = require('./middleware/globalErrorHandler');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('<h1>Store API</h1>');
+});
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl}`, 404));
+});
+// global error handling middleware
+app.use(globalErrorHandler);
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const app = require('./app');
+
+const db = process.env.DB.replace('<password>', process.env.DB_PASS);
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connected successfully');
+    const port = process.env.PORT || 3000;
+
+    app.listen(port, () => {
+      console.log(`Listening on port: ${port}`);
+    });
+  });
