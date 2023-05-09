9a67a94970cf22f2ce8fe980976e3c78a57a1d38
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-05-09 16:04:09 -0700

    sorting and limiting fields

--- controllers/productController.js
+++ controllers/productController.js
@@ -7,7 +7,22 @@ exports.getAllProducts = catchAsync(async (req, res, next) => {
   excludedFields.forEach((el) => delete queryObj[el]);
   let queryStr = JSON.stringify(queryObj);
   queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
   let query = Product.find(JSON.parse(queryStr));
+
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  if (req.query.fields) {
+    const fields = req.query.fields.split(',').join(' ');
+    query = query.select(fields);
+  } else {
+    query = query.select('-__v');
+  }
   const products = await query;
   res.status(200).send({ quantity: products.length, products });
 });
